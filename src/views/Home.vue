375d934f7629add6f2fbcc0040c67727288d802e
William Hsiao <william.hsiao.25@gmail.com>
2021-09-01 18:27:47 +0900

    Bugfix: Disappearing first panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,21 +8,22 @@
   blog
   about
 
-  .panel.footer
-    .panel-bg
+  .footer
+    .panel
+      .panel-bg
 </template>
 
 <script>
-import NavMenu from '@/components/Nav.vue'
-import Cover from '@/components/Cover.vue'
-import Socials from '@/components/Socials.vue'
-import Intro from '@/components/Intro.vue'
-import Projects from '@/components/Projects.vue'
-import Blog from '@/components/Blog.vue'
-import About from '@/components/About.vue'
+import NavMenu from "@/components/Nav.vue";
+import Cover from "@/components/Cover.vue";
+import Socials from "@/components/Socials.vue";
+import Intro from "@/components/Intro.vue";
+import Projects from "@/components/Projects.vue";
+import Blog from "@/components/Blog.vue";
+import About from "@/components/About.vue";
 
 export default {
-  name: 'home',
+  name: "home",
 
   components: {
     NavMenu,
@@ -31,23 +32,20 @@ export default {
     Intro,
     Projects,
     Blog,
-    About
+    About,
   },
 
   created() {
-    window.addEventListener('scroll', () => {
-      const socials = document.querySelector('.socials');
-      if (window.scrollY > 0) socials.classList.add('docked');
-      else socials.classList.remove('docked');
-    })
+    window.addEventListener("scroll", () => {
+      const socials = document.querySelector(".socials");
+      if (window.scrollY > 0) socials.classList.add("docked");
+      else socials.classList.remove("docked");
+    });
   },
-}
+};
 </script>
 
-<style lang='scss'>
-.container {
-  overflow: hidden;
-}
+<style lang="scss">
 .panel {
   display: flex;
   justify-content: center;
@@ -121,16 +119,17 @@ export default {
   position: absolute;
   height: 100%;
   width: 100%;
-  box-shadow: 0 30px 50px rgba(0,0,0,.25);
+  box-shadow: 0 30px 50px rgba(0, 0, 0, 0.25);
   z-index: 0;
 }
 
 .footer {
-  margin-bottom: -500px;
   margin-top: -155px;
-  height: 800px;
+  height: 350px;
+  overflow: hidden;
+
   .panel-bg {
-    background-color: #E8E8E8;
+    background-color: #e8e8e8;
     transform: skew(0, 12deg);
     top: 220px;
   }
