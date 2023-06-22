5ca7bf25ff94a33acbbf51e1c3df1acebe9fe3a7
William Hsiao <william.hsiao.25@gmail.com>
2023-06-22 20:47:07 +0900

    title: Add vertical layout & scale socials

--- src/components/pages/top/TitlePanel.vue
+++ src/components/pages/top/TitlePanel.vue
@@ -7,7 +7,7 @@
     <div class="title">
       <div class="container">
         <h1>William Hsiao</h1>
-        <Socials />
+        <Socials class="socials" />
       </div>
     </div>
   </Panel>
@@ -38,24 +38,11 @@ $transition-time: .35s;
   align-items: center;
   justify-content: center;
 
-  transition: all calc($transition-time * 1.5) linear $transition-time;
-
-
-  &:deep(.bg) {
-
-    .bg-main,
-    .bg-sub {
-      transition: all calc($transition-time / 2) linear $transition-time;
-    }
-  }
-
   &:deep(.content) {
     display: flex;
     align-items: center;
     justify-content: center;
     position: relative;
-
-    transition: min-width $transition-time linear;
   }
 
   .logo {
@@ -66,20 +53,45 @@ $transition-time: .35s;
   }
 
   .title {
-    max-width: 100%;
-    opacity: 1;
-    text-align: right;
     overflow: hidden;
-    transition: max-width $transition-time linear,
-      opacity calc($transition-time * 1.5) linear;
-
-    .container {
-      padding-left: 10vw;
-    }
 
     h1 {
       color: $color-background-primary-contrast;
       white-space: nowrap;
+    }
+
+    .socials:deep(img) {
+      height: 4vh;
+    }
+  }
+}
+
+// Layout and transition styles
+.title-panel {
+  transition: all calc($transition-time * 1.5) linear $transition-time;
+
+  &:deep(.bg) {
+
+    .bg-main,
+    .bg-sub {
+      transition: all calc($transition-time / 2) linear $transition-time;
+    }
+  }
+
+  &:deep(.content) {
+    transition: min-width $transition-time linear;
+  }
+
+  .title {
+    max-width: 100%;
+    opacity: 1;
+    transition:
+      max-height $transition-time linear,
+      max-width $transition-time linear,
+      opacity calc($transition-time * 1.5) linear;
+
+    .container {
+      padding-left: 10vw;
     }
   }
 
@@ -104,5 +116,27 @@ $transition-time: .35s;
     }
 
   }
+}
+
+@media (max-width: 480px) {
+  .title-panel:deep(.content) {
+    flex-direction: column;
+
+    .title {
+      max-height: 100vh;
+
+      .container {
+        padding-left: 0;
+      }
+    }
+  }
+
+  .title-panel.transition-in:deep(.content) {
+    .title {
+      max-width: 100%;
+      max-height: 0;
+    }
+  }
+
 }
 </style>
