db973385eae2222c29bc7fde9234beb5da6f590f
William Hsiao <william.hsiao.25@gmail.com>
2023-06-25 20:16:01 +0900

    app: Set CSP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ["@nuxt/content", "@nuxtjs/google-fonts"],
+  modules: ["@nuxt/content", "@nuxtjs/google-fonts", "nuxt-security"],
 
   googleFonts: {
     families: {
@@ -32,5 +32,33 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
     strict: true,
+  },
+
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        "default-src": ["'self'"],
+        "script-src": [
+          "'self'",
+          "https://*.googletagmanager.com",
+          "'unsafe-inline'",
+        ],
+        "style-src": ["'self'", "'unsafe-inline'"],
+        "connect-src": [
+          "'self'",
+          "https://*.google-analytics.com",
+          "https://*.analytics.google.com",
+          "https://*.googletagmanager.com",
+          ...(process.env.NODE_ENV === "development"
+            ? ["ws://localhost:*", "http://localhost:*"]
+            : []),
+        ],
+        "img-src": [
+          "'self'",
+          "https://*.google-analytics.com",
+          "https://*.googletagmanager.com",
+        ],
+      },
+    },
   },
 });
